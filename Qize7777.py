5f474dadd920acb0bdd7b12e0e26e4934ce307ac
WonJoong <realjoong@gmail.com>
2020-04-28 15:36:27 +0900

    commit #1
    * 사진 파일을 먼저 추가하지 않고 사진 수정을 시도했을 때 오류 메세지박스 출력

--- Qize7777.py
+++ Qize7777.py
@@ -1,38 +1,42 @@
 from tkinter import *
 from tkinter.filedialog import *
 from tkinter.simpledialog import *
+from tkinter import messagebox
 import os.path
 import math
 
+
 ## 함수 선언 부분 ##
-def loadImage(fname) :
+def loadImage(fname):
     global filename, inImage, canvas, paper, window, XSIZE, YSIZE
 
     inImage = []
     fsize = os.path.getsize(fname)
-    XSIZE=YSIZE=int(math.sqrt(fsize))
+    XSIZE = YSIZE = int(math.sqrt(fsize))
 
     fp = open(fname, 'rb')
 
-    for i in range(0, XSIZE) :
+    for i in range(0, XSIZE):
         tmpList = []
-        for k in range(0, YSIZE) :
+        for k in range(0, YSIZE):
             data = int(ord(fp.read(1)))
             tmpList.append(data)
         inImage.append(tmpList)
 
     fp.close()
 
-def displayImage() :
+
+def displayImage():
     global XSIZE, YSIZE, inImage
     rgbString = ""
-    for i in range(0, XSIZE) :
+    for i in range(0, XSIZE):
         tmpString = ""
-        for k in range(0, YSIZE) :
+        for k in range(0, YSIZE):
             data = inImage[i][k]
-            tmpString += "#%02x%02x%02x " % (data, data, data) # x 뒤에 한칸 공백
-        rgbString += "{" + tmpString +  "} " # } 뒤에 한칸 공백
+            tmpString += "#%02x%02x%02x " % (data, data, data)  # x 뒤에 한칸 공백
+        rgbString += "{" + tmpString + "} "  # } 뒤에 한칸 공백
     paper.put(rgbString)
+
 
 def func_open():
     global filename, inImage, canvas, paper, window, XSIZE, YSIZE
@@ -40,7 +44,7 @@ def func_open():
     # 파일 --> 메모리
     filename = askopenfilename(parent=window, filetypes=(("RAW 파일", "*.raw"), ("모든 파일", "*.*")))
 
-    if filename =="":
+    if filename == "":
         return
     if canvas != None:
         canvas.destroy()
@@ -56,59 +60,75 @@ def func_open():
 
     canvas.pack()
 
+
 def func_exit():
     window.quit()
     window.destroy()
 
+
 def Make_B():
-    global XSIZE, YSIZE,inImage
-    rgbString = ""
-    for i in range(0, XSIZE) :
-        tmpString = ""
-        for k in range(0, YSIZE) :
-            data = inImage[i][k]
-            if (data+128)>256 >0:
-                data=255
-            tmpString += "#%02x%02x%02x " % (data,data,data) # x 뒤에 한칸 공백
-        rgbString += "{" + tmpString +  "} " # } 뒤에 한칸 공백
-    paper.put(rgbString)
+    try:
+        while True:
+            global XSIZE, YSIZE, inImage
+            rgbString = ""
+            for i in range(0, XSIZE):
+                tmpString = ""
+                for k in range(0, YSIZE):
+                    data = inImage[i][k]
+                    if (data + 128) > 256 > 0:
+                        data = 255
+                    tmpString += "#%02x%02x%02x " % (data, data, data)  # x 뒤에 한칸 공백
+                rgbString += "{" + tmpString + "} "  # } 뒤에 한칸 공백
+            paper.put(rgbString)
+    except IndexError:
+        messagebox.showinfo(title="ERROR!", message="사진을 먼저 추가해주세요.")
+
 
 def Make_D():
-    global XSIZE, YSIZE,inImage
-    rgbString = ""
-    for i in range(0, XSIZE) :
-        tmpString = ""
-        for k in range(0, YSIZE) :
-            data = inImage[i][k]
-            if (data-128)<0:
-                data=0
-            tmpString += "#%02x%02x%02x " % (data,data,data) # x 뒤에 한칸 공백
-        rgbString += "{" + tmpString +  "} " # } 뒤에 한칸 공백
-    paper.put(rgbString)
+    try:
+        while True:
+            global XSIZE, YSIZE, inImage
+            rgbString = ""
+            for i in range(0, XSIZE):
+                tmpString = ""
+                for k in range(0, YSIZE):
+                    data = inImage[i][k]
+                    if (data - 128) < 0:
+                        data = 0
+                    tmpString += "#%02x%02x%02x " % (data, data, data)  # x 뒤에 한칸 공백
+                rgbString += "{" + tmpString + "} "  # } 뒤에 한칸 공백
+            paper.put(rgbString)
+    except IndexError:
+        messagebox.showinfo(title="ERROR!", message="사진을 먼저 추가해주세요.")
+
 
 def Make_deca():
-    global XSIZE, YSIZE,inImage
-    rgbString = ""
-    for i in range(0, XSIZE) :
-        tmpString = ""
-        for k in range(0, YSIZE) :
-            data = inImage[i][k]
-            tmpString += "#%02x%02x%02x " % (255-data, 255-data, 255-data) # x 뒤에 한칸 공백
-        rgbString += "{" + tmpString +  "} " # } 뒤에 한칸 공백
-    paper.put(rgbString)
+    try:
+        while True:
+            global XSIZE, YSIZE, inImage
+            rgbString = ""
+            for i in range(0, XSIZE):
+                tmpString = ""
+                for k in range(0, YSIZE):
+                    data = inImage[i][k]
+                    tmpString += "#%02x%02x%02x " % (255 - data, 255 - data, 255 - data)  # x 뒤에 한칸 공백
+                rgbString += "{" + tmpString + "} "  # } 뒤에 한칸 공백
+            paper.put(rgbString)
+    except IndexError:
+        messagebox.showinfo(title="ERROR!", message="사진을 먼저 추가해주세요.")
 
 
 ## 전역 변수 선언 부분 ##
 window = None
 canvas = None
-XSIZE, YSIZE=255,255
-inImage=[] # 2차원 리스트 (메모리)
+XSIZE, YSIZE = 255, 255
+inImage = []  # 2차원 리스트 (메모리)
 
 ## 메인 코드 부분 ##
 if __name__ == "__main__":
     window = Tk()
     window.title("흑백 사진 보기")
-    mainMenu =Menu(window)
+    mainMenu = Menu(window)
     window.config(menu=mainMenu)
 
     fileMenu1 = Menu(mainMenu)
