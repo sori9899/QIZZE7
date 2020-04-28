559817f9eeb0228235f4332c41736927dfeca4c6
yu-podong <62629742+yu-podong@users.noreply.github.com>
2020-04-28 18:58:25 +0900

    전반적인 코드 수정
    함수 내의 불필요한 글로벌 변수 제거
    data값 변경 부분 수정
    paper 전역 변수 추가

--- Qize7777.py
+++ Qize7777.py
@@ -7,27 +7,46 @@ import math
 
 
 ## 함수 선언 부분 ##
+def func_open():
+    global inImage, canvas, paper, window, XSIZE, YSIZE
+
+    # 파일 --> 메모리
+    filename = askopenfilename(parent=window, filetypes=(("RAW 파일", "*.raw"), ("모든 파일", "*.*")))
+
+    if filename == "":  # 파일을 불러오는 중 취소
+        return
+    if canvas != None:  # 두 번 이상 새로운 파일을 불러오는 경우 실행
+        canvas.destroy()
+
+    # 캔버스 생성 (아직 점을 찍을 종이만 생성, 아직 사진 삽입X)
+    canvas = Canvas(window, height=XSIZE, width=YSIZE)
+    paper = PhotoImage(width=XSIZE, height=YSIZE)
+    canvas.create_image((XSIZE / 2, YSIZE / 2), image=paper, state="normal")
+
+    loadImage(filename)  # RAW 파일 데이터 저장
+    displayImage()  # 메모리 --> 화면
+
+    canvas.pack()
+
 def loadImage(fname):
-    global filename, inImage, canvas, paper, window, XSIZE, YSIZE
-
+    global inImage, XSIZE, YSIZE
     inImage = []
-    fsize = os.path.getsize(fname)
-    XSIZE = YSIZE = int(math.sqrt(fsize))
 
+    fsize = os.path.getsize(fname)  # 파일의 크키
+    XSIZE = YSIZE = int(math.sqrt(fsize))  # 사진의 행과 열이 같다고 가정 (256 X 256)
     fp = open(fname, 'rb')
 
     for i in range(0, XSIZE):
-        tmpList = []
+        tmpList = []  # inImage의 한 행을 구성할 리스트
         for k in range(0, YSIZE):
-            data = int(ord(fp.read(1)))
-            tmpList.append(data)
-        inImage.append(tmpList)
+            data = int(ord(fp.read(1)))  # 파일을 1byte씩 읽은 후, 고유값을 int형으로 변경
+            tmpList.append(data)  # tmpLIst에 순차적으로 추가
+        inImage.append(tmpList)  # tmpList가 한 행을 구성했다는 그 리스트를 inimage에 주가하여 행렬 구성
 
     fp.close()
 
-
 def displayImage():
-    global XSIZE, YSIZE, inImage
+    global XSIZE, YSIZE, inImage, paper
     rgbString = ""
     for i in range(0, XSIZE):
         tmpString = ""
@@ -35,30 +54,7 @@ def displayImage():
             data = inImage[i][k]
             tmpString += "#%02x%02x%02x " % (data, data, data)  # x 뒤에 한칸 공백
         rgbString += "{" + tmpString + "} "  # } 뒤에 한칸 공백
-    paper.put(rgbString)
-
-
-def func_open():
-    global filename, inImage, canvas, paper, window, XSIZE, YSIZE
-
-    # 파일 --> 메모리
-    filename = askopenfilename(parent=window, filetypes=(("RAW 파일", "*.raw"), ("모든 파일", "*.*")))
-
-    if filename == "":
-        return
-    if canvas != None:
-        canvas.destroy()
-
-    canvas = Canvas(window, height=XSIZE, width=YSIZE)
-    paper = PhotoImage(width=XSIZE, height=YSIZE)
-    canvas.create_image((XSIZE / 2, YSIZE / 2), image=paper, state="normal")
-
-    loadImage(filename)
-
-    # 메모리 --> 화면
-    displayImage()
-
-    canvas.pack()
+    paper.put(rgbString)  # 사용자에게 사진을 보여줌
 
 
 def func_exit():
@@ -74,8 +70,8 @@ def Make_B():
             for i in range(0, XSIZE):
                 tmpString = ""
                 for k in range(0, YSIZE):
-                    data = inImage[i][k]
-                    if (data + 128) > 256 > 0:
+                    data = inImage[i][k] + 128
+                    if data > 255:  # 최대 범위인 255를 넘어가는 데이터의 값을 255로 설정
                         data = 255
                     tmpString += "#%02x%02x%02x " % (data, data, data)  # x 뒤에 한칸 공백
                 rgbString += "{" + tmpString + "} "  # } 뒤에 한칸 공백
@@ -92,8 +88,8 @@ def Make_D():
             for i in range(0, XSIZE):
                 tmpString = ""
                 for k in range(0, YSIZE):
-                    data = inImage[i][k]
-                    if (data - 128) < 0:
+                    data = inImage[i][k] - 128
+                    if data < 0:  # 최소 범위인 0보다 작은 데이터의 값을 0으로 설정
                         data = 0
                     tmpString += "#%02x%02x%02x " % (data, data, data)  # x 뒤에 한칸 공백
                 rgbString += "{" + tmpString + "} "  # } 뒤에 한칸 공백
@@ -110,8 +106,8 @@ def Make_deca():
             for i in range(0, XSIZE):
                 tmpString = ""
                 for k in range(0, YSIZE):
-                    data = inImage[i][k]
-                    tmpString += "#%02x%02x%02x " % (255 - data, 255 - data, 255 - data)  # x 뒤에 한칸 공백
+                    data = 255 - inImage[i][k]  # 흑백 반전
+                    tmpString += "#%02x%02x%02x " % (data, data, data)  # x 뒤에 한칸 공백
                 rgbString += "{" + tmpString + "} "  # } 뒤에 한칸 공백
             paper.put(rgbString)
     except IndexError:
@@ -119,9 +115,8 @@ def Make_deca():
 
 
 ## 전역 변수 선언 부분 ##
-window = None
-canvas = None
-XSIZE, YSIZE = 255, 255
+window, canvas, paper = None, None, None  # 세 개의 변수를 None으로 설정
+XSIZE, YSIZE = 256, 256
 inImage = []  # 2차원 리스트 (메모리)
 
 ## 메인 코드 부분 ##
